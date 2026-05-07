65982b98883a32a1646de128c78868a46ae1d875
Nardi <nardisuribel@gmail.com>
2026-05-07 11:50:07 -0600

    add dumb changes to do squash commit

--- src/main/java/com/agendapp/agenda/controller/Test2Interface.java
+++ src/main/java/com/agendapp/agenda/controller/Test2Interface.java
@@ -3,6 +3,6 @@ package com.agendapp.agenda.controller;
 public interface Test2Interface extends TestInterface {
     
     default void testmethod(){
-        
+        System.out.println("testmethod");
     }
 }
